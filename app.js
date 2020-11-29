656c225bc20be8c349599774c5125e3172df038a
Briley Hanson <hansonb7@spu.edu>
2020-11-29 12:46:12 -0800

    removed cors

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-const cors = require('cors');
+
 
 
 
@@ -12,7 +12,6 @@ var indexRouter = require('./routes/index');
 
 var app = express();
 // view engine setup
-app.use(cors);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
