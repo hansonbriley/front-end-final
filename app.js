f3bdb2051e3412a9db91ddb16c4d854e9c78879d
Briley Hanson <hansonb7@spu.edu>
2020-11-29 12:52:20 -0800

    installed and added cors

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var cors = require('cors');
+
 
 
 
@@ -12,6 +14,7 @@ var indexRouter = require('./routes/index');
 
 var app = express();
 // view engine setup
+app.use(cors());
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
