9d040eb14e1b8c4791f90ea28b5f9eb2a2fde10a
Briley Hanson <hansonb7@spu.edu>
2020-11-29 13:00:01 -0800

    cors changes

--- app.js
+++ app.js
@@ -13,7 +13,6 @@ var indexRouter = require('./routes/index');
 
 
 var app = express();
-// view engine setup
 app.use(cors());
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
