a11e424e0af0500c7ce810c32519e576d3ff6318
Briley Hanson <hansonb7@spu.edu>
2020-11-29 12:36:27 -0800

    working on get requests

--- app.js
+++ app.js
@@ -5,13 +5,12 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const cors = require('cors');
 
-app.use(cors);
+
 
 var indexRouter = require('./routes/index');
 
 
 var app = express();
-
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
--- public/javascripts/books.js
+++ public/javascripts/books.js
@@ -2,26 +2,65 @@
 
 function PostBook()
 {
-    
+    console.log("Step 1");
     let name = document.getElementById('bookName').value;
     let author = document.getElementById('bookAuthor').value;
     let isbn = document.getElementById('bookISBN').value;
     let price = document.getElementById('bookPrice').value;
-    let params = 'name=' + name + '&author=' + author + '&isbn=' + isbn + '&price=' + price;
 
     let xhttp = new XMLHttpRequest();
-    xhttp.open("POST", 'https://hidden-plateau-59052.herokuapp.com/api/books', true);
-
-//Send the proper header information along with the request
+    xhttp.open("POST", 'https://hidden-plateau-59052.herokuapp.com/api/books/', true);
     xhttp.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+    xhttp.send(JSON.stringify({
+        name: name,
+        author: author,
+        isbn: isbn,
+        price: price,
+    }));
+    console.log("Data Sent");
+}
 
-    xhttp.onreadystatechange = function ()
-    {
-        if (xhttp.readyState == 4 && xhttp.status == 200)
-        {
-            alert(xhttp.responseText);
+function GetBook(){
+    console.log("Step 1");
+    let xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function ReceivedCallback() {
+        if (this.readyState == 4 && this.status == 200) {
+            document.getElementById("output").innerHTML = CreateTable(JSON.parse(this.responseText));
         }
-    }
+    };
+    xhttp.open("GET", "https://hidden-plateau-59052.herokuapp.com/api/books", true);
+    xhttp.send();
+}
 
-    xhttp.send(params);
+function CreateTable(books)
+{
+    let retVal = '';
+    retVal =
+        '<div class="jumbotron"> \n' +
+        '<table class="table table-bordered table-hover"> \n' +
+        '	<thead> \n' +
+        '		<tr> \n' +
+        '			<th scope="col">Title</th> \n' +
+        '			<th scope="col">Author</th> \n' +
+        '			<th scope="col">ISBN</th> \n' +
+        '			<th scope="col">Price</th> \n' +
+        '		</tr> \n' +
+        '	</thead> \n' +
+        '	<tbody> \n';
+
+    for (let book in books){
+        retVal +=
+            '<tr> \n' +
+            '	<td>' + books["name"] + '</td> \n' +
+            '	<td>' + books["author"] + '</td> \n' +
+            '	<td>' + books["isbn"] + '</td> \n' +
+            '	<td>' + books["price"] + '</td> \n' +
+            '</tr> \n';
+
+    }
+    retVal +=
+        '</tbody> \n' +
+        '</table> \n' +
+        '</div> \n ' ;
+    return retVal;
 }
